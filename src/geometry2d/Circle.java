25a23db6f68bf54337a95144b3454ad5dff095e1
tr1zet <uliskin06@mail.ru>
2025-09-28 19:03:16 +0700

    feat(lab2): implement Circle and Cylinder and Figure and GeometryTask and Rectangle. Сhecking for errors(SimplException). Changes(README.md)

---
+++ src/geometry2d/Circle.java
@@ -0,0 +1,24 @@
+package geometry2d;
+
+public class Circle implements Figure {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Круг [r=" + radius + "]";
+    }
+}
