da1bc6beeccdeff470df39476e499af15e6e072f
tr1zet <uliskin06@mail.ru>
2025-10-19 17:57:04 +0700

    feat(lab5):  implement(task1) EvenThread and ThreadCreationDemo and OddRunnable

---
+++ src/task1/EvenThread.java
@@ -0,0 +1,22 @@
+package task1;
+
+public class EvenThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " начал работу (чётные числа):");
+
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+            try {
+                // Имитация работы потока
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.err.println("Поток был прерван: " + e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        System.out.println(Thread.currentThread().getName() + " завершил работу.");
+    }
+}
