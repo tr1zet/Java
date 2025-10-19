2977fa62da6e9a8102ac01d52baec014c0a2b460
tr1zet <uliskin06@mail.ru>
2025-10-19 18:47:15 +0700

    feat(lab5): fix EvenThread and ProducerConsumerDemo

--- src/task1/EvenThread.java
+++ src/task1/EvenThread.java
@@ -8,15 +8,15 @@ public class EvenThread extends Thread {
 
         for (int i = 2; i <= 10; i += 2) {
             System.out.println(Thread.currentThread().getName() + ": " + i);
-            try {
-                // Имитация работы потока
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                System.err.println("Поток был прерван: " + e.getMessage());
-                Thread.currentThread().interrupt();
-            }
+            Thread.ofVirtual().start(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.err.println("Поток был прерван: " + e.getMessage());
+                    Thread.currentThread().interrupt();
+                }
+            });
         }
-
         System.out.println(Thread.currentThread().getName() + " завершил работу.");
     }
 }
