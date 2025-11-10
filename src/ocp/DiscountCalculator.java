2041279abce9169b40c28d289a85183a4eb299b7
tr1zet <148443756+tr1zet@users.noreply.github.com>
2025-11-10 11:01:35 +0700

    Delete src directory

--- src/ocp/DiscountCalculator.java
+++
@@ -1,7 +0,0 @@
-package ocp;
-
-public class DiscountCalculator {
-    public double calculateDiscount(DiscountStrategy strategy, double price) {
-        return strategy.calculateDiscount(price);
-    }
-}
