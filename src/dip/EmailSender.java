107d3ba0909c6bd4aebf24dbb14c01e752709b3c
tr1zet <148443756+tr1zet@users.noreply.github.com>
2025-11-23 22:42:07 +0700

    Delete src/dip directory

--- src/dip/EmailSender.java
+++
@@ -1,8 +0,0 @@
-package dip;
-
-public class EmailSender implements MessageSender {
-    @Override
-    public void send(String message) {
-        System.out.println("Отправка письма: " + message);
-    }
-}
--- src/dip/SmsSender.java
+++
@@ -1,8 +0,0 @@
-package dip;
-
-public class SmsSender implements MessageSender {
-    @Override
-    public void send(String message) {
-        System.out.println("Отправка SMS: " + message);
-    }
-}
