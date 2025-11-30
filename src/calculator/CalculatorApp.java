4bb780cffa50914f25c7ff99ea23c93ca897f98e
tr1zet <uliskin06@mail.ru>
2025-11-30 16:48:26 +0700

    feat(lab10): correct CalculatorApp and and GeometryApp and impliment AppLauncher

--- src/calculator/CalculatorApp.java
+++ src/calculator/CalculatorApp.java
@@ -1,30 +1,18 @@
 package calculator;
 
+import common.AppLauncher;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class CalculatorApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/calculator/calculator.fxml"));
-            Parent root = loader.load();
-
-            primaryStage.setTitle("Калькулятор");
-            Scene scene = new Scene(root, 400, 500);
-            primaryStage.setScene(scene);
-            primaryStage.setMinWidth(400);
-            primaryStage.setMinHeight(500);
-            primaryStage.show();
-
-        } catch (Exception e) {
-            // В production коде не показываем stack trace
-            System.err.println("Не удалось запустить калькулятор");
-        }
+        AppLauncher.launchApp(primaryStage,
+                "/calculator/calculator.fxml",
+                "Калькулятор",
+                400, 500, 400, 500
+        );
     }
 
     public static void main(String[] args) {
--- src/geometry/GeometryApp.java
+++ src/geometry/GeometryApp.java
@@ -1,29 +1,18 @@
 package geometry;
 
+import common.AppLauncher;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GeometryApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/geometry/geometry.fxml"));
-            Parent root = loader.load();
-
-            primaryStage.setTitle("Геометрические фигуры");
-            Scene scene = new Scene(root, 800, 600);
-            primaryStage.setScene(scene);
-            primaryStage.setMinWidth(600);
-            primaryStage.setMinHeight(400);
-            primaryStage.show();
-
-        } catch (Exception e) {
-            System.err.println("Ошибка загрузки приложения: " + e.getMessage());
-        }
+        AppLauncher.launchApp(primaryStage,
+                "/geometry/geometry.fxml",
+                "Геометрические фигуры",
+                800, 600, 600, 400
+        );
     }
 
     public static void main(String[] args) {
